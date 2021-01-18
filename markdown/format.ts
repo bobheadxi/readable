815586e39f645e69b309493bd9e8abfbb20bed96
Robert Lin <robert@bobheadxi.dev>
2021-01-18 21:17:27 +0800

    fix plugins

--- markdown/format.ts
+++ markdown/format.ts
@@ -1,12 +1,9 @@
 import remark from "./remark.ts";
-import plugins from "../plugins/mod.ts";
 
 // formatting configuration
 export default function format(markdown: string): string {
   let formatted = markdown;
-  remark
-    .use(plugins)
-    .process(markdown, (err: any, file: any) => {
+  remark.process(markdown, (err: any, file: any) => {
       if (err) throw err;
       formatted = String(file);
     });
--- markdown/remark.ts
+++ markdown/remark.ts
@@ -1,3 +1,10 @@
+import plugins from "../plugins/mod.ts";
+
 import remarkImport from "https://jspm.dev/remark@13.0.0";
-const remark = (remarkImport as any)();
+let remark = (remarkImport as any)();
+
+for (const p of plugins) {
+    remark = remark.use(p)
+}
+
 export default remark;
