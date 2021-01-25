a1b7cb5e79bdd59d12c932fcab82f90c3a285946
Robert Lin <robert@bobheadxi.dev>
2021-01-25 22:36:02 +0800

    check import.meta.main before running command

--- readable.ts
+++ readable.ts
@@ -24,22 +24,28 @@ cli.command("check [...globs]", "Check Markdown formatting")
   });
 
 /**
- * Show help if no args are provided, or on help flags, or on exit (?)
+ * Show help message on -h and --help flags
  */
 cli.help();
-if (Deno.args.length == 0) {
-  cli.outputHelp();
-  Deno.exit(1);
-}
-
+ 
 /**
- * Run input commands
+ * Run input commands if run as a script.
+ * 
+ * See https://deno.land/manual@master/tools/script_installer
  */
-try {
-  cli.parse();
-} catch (err) {
-  console.error(`${err.message}`);
-  Deno.exit(1);
+if (import.meta.main) {
+  // Show help if no args are provided
+  if (Deno.args.length == 0) {
+    cli.outputHelp();
+    Deno.exit(1);
+  }
+
+  try {
+    cli.parse();
+  } catch (err) {
+    console.error(`${err.message}`);
+    Deno.exit(1);
+  }
 }
 
-export {};
+export default cli;
