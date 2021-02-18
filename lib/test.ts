4df4b931ab9f736813eb97051dda89cd5be12294
Robert Lin <robert@bobheadxi.dev>
2021-02-18 17:32:10 +0800

    test: place loren ipsum fixture in shared

--- lib/test.ts
+++ lib/test.ts
@@ -63,3 +63,24 @@ export class TestSuite<InputT, OutputT> {
     });
   }
 }
+
+const lorenIpsum = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+];
+
+/**
+ * Each line is a sentence.
+ */
+export function lorenIpsumLines() {
+  return [...lorenIpsum];
+}
+
+/**
+ * Places all sentences on a single line.
+ */
+export function lorenIpsumText() {
+  return lorenIpsum.join(" ");
+}
--- plugins/readable/reflow.test.ts
+++ plugins/readable/reflow.test.ts
@@ -4,7 +4,7 @@ import format from "../../markdown/format.ts";
 import { diff } from "../../lib/diff.ts";
 
 import reflow from "./reflow.ts";
-import { TestSuite } from "../../lib/test.ts";
+import { lorenIpsumLines, lorenIpsumText, TestSuite } from "../../lib/test.ts";
 
 // e2e tests just run a format
 {
@@ -17,22 +17,20 @@ import { TestSuite } from "../../lib/test.ts";
   }, [
     {
       case: "should split Lorem Ipsum",
-      input:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-      expect:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-`,
+      input: lorenIpsumText(),
+      expect: lorenIpsumLines().join("\n") + "\n",
     },
     {
       case: "break sentences that start with an unbreakable node",
-      input:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. [now here is a link](https://bobheadxi.dev) in a rather long sentence, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \`and then some inline code\` in another sentence.`,
-      expect:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-[now here is a link](https://bobheadxi.dev) in a rather long sentence, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      input: `${
+        lorenIpsumLines()[0]
+      } [now here is a link](https://bobheadxi.dev) in a rather long sentence, ${
+        lorenIpsumLines()[1]
+      } \`and then some inline code\` in another sentence.`,
+      expect: `${lorenIpsumLines()[0]}
+[now here is a link](https://bobheadxi.dev) in a rather long sentence, ${
+        lorenIpsumLines()[1]
+      }
 \`and then some inline code\` in another sentence.
 `,
     },
